c652655698082b336d50ed03986708ee4ab4238b
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2022-07-27 17:45:52 -0300

    change formatting for printed text

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	const conferenceTicketsNum = 50
 	var remainingTickets = conferenceTicketsNum
 
-	fmt.Print("Welcome to the " + conferenceName + " booking application.\n")
+	fmt.Printf("Welcome to the %v booking application.\n", conferenceName)
 	fmt.Print("There are ", remainingTickets, " tickets remaining, out of a total of ", remainingTickets, " tickets. \n")
 	fmt.Print("Get your tickets here to attend.")
 }
