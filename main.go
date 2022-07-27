ee0a4122c26c86d30841c3b3fdd455ede1e54ce1
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2022-07-27 16:12:31 -0300

    Add greeting message

--- main.go
+++ main.go
@@ -4,5 +4,6 @@ import "fmt"
 
 // Where to start the program - compiler signal
 func main() {
-	fmt.Print("Hello, World.")
+	fmt.Print("Welcome to our conference booking application.")
+	fmt.Print("Get your tickets here to attend.")
 }
