e04ee01b227e4d335d348d459d4770b09b9fa8f2
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2022-07-27 17:14:55 -0300

    Print remaining tickets with welcome message

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // Where to start the program - compiler signal
 func main() {
 	var conferenceName = "Go Conference"
+	const conferenceTicketsNum = 50
+	var remainingTickets = conferenceTicketsNum
 
 	fmt.Print("Welcome to the " + conferenceName + " booking application.\n")
+	fmt.Print("There are ", remainingTickets, " tickets remaining, out of a total of ", remainingTickets, " tickets. \n")
 	fmt.Print("Get your tickets here to attend.")
 }
