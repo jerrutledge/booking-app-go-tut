5b58abe637085480feac9025414de6bdf3dec64b
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2022-08-05 21:12:24 -0300

    refactor definitions into main function

--- main.go
+++ main.go
@@ -5,16 +5,16 @@ import (
 	"time"
 )
 
-func worker(id int, jobs <-chan int, results chan<- int) {
-	for j := range jobs {
-		fmt.Println("worker", id, "started  job", j)
-		time.Sleep(time.Second)
-		fmt.Println("worker", id, "finished job", j)
-		results <- j * 2
-	}
-}
-
 func main() {
+
+	worker := func(id int, jobs <-chan int, results chan<- int) {
+		for j := range jobs {
+			fmt.Println("worker", id, "started  job", j)
+			time.Sleep(time.Second)
+			fmt.Println("worker", id, "finished job", j)
+			results <- j * 2
+		}
+	}
 
 	const numJobs = 5
 	jobs := make(chan int, numJobs)
