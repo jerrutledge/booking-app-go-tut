dd41043792549a184250d204a99c2d188c029206
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2022-08-07 19:34:36 -0300

    solve basic test cases

--- main.go
+++ main.go
@@ -2,34 +2,71 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"math"
 )
 
-func main() {
+func longestPalindrome(s string) string {
 
-	worker := func(id int, jobs <-chan int, results chan<- int) {
+	centered_palindrome := func(center int, s string) string {
+		longest := s[center : center+1]
+		// two types of palindromes, odd and even
+		// ODD
+		iterrange := math.Min(float64(center), float64(len(s)-center))
+		for i := 1; i < int(iterrange); i++ { // odd, this character is the center
+			if s[center-i:center-i+1] == s[center+i:center+i+1] {
+				// still a palindrome
+				longest = s[center-i : center+i+1]
+			} else {
+				// not a palindrome anymore
+				break
+			}
+		}
+		// EVEN
+		for i := 1; i <= int(iterrange); i++ {
+			if s[center-i:center-i+1] == s[center+i-1:center+i] {
+				// still a palindrome
+				if len(s[center-i:center+i]) > len(longest) {
+					longest = s[center-i : center+i]
+				}
+			} else {
+				// not a palindrome anymore
+				break
+			}
+		}
+		return longest
+	}
+
+	// use a pool of workers to concurrently calculate palindromes
+	worker := func(index int, s string, jobs <-chan int, results chan<- string) {
 		for j := range jobs {
-			fmt.Println("worker", id, "started  job", j)
-			time.Sleep(time.Second)
-			fmt.Println("worker", id, "finished job", j)
-			results <- j * 2
+			results <- centered_palindrome(j, s)
 		}
 	}
-
-	const numJobs = 5
+	numJobs := len(s)
 	jobs := make(chan int, numJobs)
-	results := make(chan int, numJobs)
+	results := make(chan string, numJobs)
 
 	for w := 1; w <= 3; w++ {
-		go worker(w, jobs, results)
+		go worker(w, s, jobs, results)
 	}
 
-	for j := 1; j <= numJobs; j++ {
+	for j := 0; j < numJobs; j++ {
 		jobs <- j
 	}
 	close(jobs)
 
+	longest := ""
 	for a := 1; a <= numJobs; a++ {
-		<-results
+		str := <-results
+		if len(str) > len(longest) {
+			longest = str
+		}
 	}
+	return longest
+}
+
+func main() {
+
+	s := "ccc"
+	fmt.Println(longestPalindrome(s))
 }
